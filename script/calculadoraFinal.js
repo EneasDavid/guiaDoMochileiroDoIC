9bb3dad7a1fc1c7442cc8c29a62b4269285b4c06
Enéas <david00eneas@gmail.com>
2023-10-26 10:24:58 -0300

    Front end inicial para o projeto e integração com o backend

--- script/calculadoraFinal.js
+++ script/calculadoraFinal.js
@@ -1,46 +1,57 @@
 let media=0;
-let notaFinalAluno=0;
+let notaFinalAluno = 0;
+let notaUm = document.querySelector('input[name="notaUm"]');
+let notaDois = document.querySelector('input[name="notaDois"]');
+const valorMedia = document.querySelector("[data-valor-mediaRegular]");
+const valorNotaFinal = document.querySelector("[data-valor-notaFinal]");
 
-// Selecionar os elementos
-const notaUm=document.querySelector("[data-notaUm-input]");
-const notaDois=document.querySelector("[data-notaDois-input]");
-const valorMedia=document.querySelector("[data-valor-mediaRegular]");
-const valorNotaFinal=document.querySelector("[data-valor-NotaFinal]");
-
-function calcMedia(){
-    media=(parseFloat(notaUm.value)+parseFloat(notaDois.value))/2;
-    valorMedia.textContent=media;
+function limpar(campo){
+    // Limpa o conteúdo anterior
+    campo.innerHTML='';
+}
+function calcMedia() {
+    media=(parseFloat(notaUm.value)+parseFloat(notaDois.value))/2;   
+    // Cria uma string HTML com as mensagens da média
+    const mensagemMediaHTML="<span>Sua média regular é: </span><span class='regular'>" + media.toFixed(2) + "</span>";
+    valorMedia.innerHTML=mensagemMediaHTML;
 }
 
-function calculaFinal(){
+function calculaFinal() {
     const mediaFinal=5.5;
     notaFinalAluno=(mediaFinal-(media*.6))/.4;
-    valorNotaFinal.textContent = notaFinalAluno;
+    // Cria uma string HTML com as mensagens da nota final
+    const mensagemNotaFinalHTML="<span>Você precisa tirar: </span><span class='alerta'>" + notaFinalAluno.toFixed(2) + "</span>";
+    valorNotaFinal.innerHTML=mensagemNotaFinalHTML;
 }
 
-function executa(){
+function executa() {
     // Verificar se os campos de nota estão vazios
-    if (notaUm.value.trim()==='' || notaDois.value.trim()===''){
-        valorMedia.textContent="";
-        valorNotaFinal.textContent="Digite ambas as notas para podermos calcular";
+    if (notaUm.value.trim() === '' || notaDois.value.trim() === '') {
+        const mensagemMediaHTML="<span><i class='regular'>Insira as duas notas</i> para calcular a média regular!</span>";
+        valorMedia.innerHTML = mensagemMediaHTML;
         return; // Sai da função se um ou ambos os campos estiverem vazios
-    }
-    // Função para executar as funções de cálculo
-    calcMedia();
-    const final=media>=5&&media<7?1:0;
-    if(final){
-        calculaFinal();
+    }else if(parseFloat(notaDois.value)<0||parseFloat(notaDois.value)>10 || parseFloat(notaUm.value)<0||parseFloat(notaUm.value)>10){
+        const mensagemMediaHTML="<span><i class='alerta'>Insira notas válidas</i> para calcular a média regular!</span>";
+        valorMedia.innerHTML=mensagemMediaHTML;
+        return;
     }else{
-        if(media<5){
-            valorNotaFinal.textContent="Infelizmente você não pode fazer a final!";
-        }else{
-            valorNotaFinal.textContent="Parabéns, você conseguiu passar com a média regular!";
-        }
+        // Função para executar as funções de cálculo
+        calcMedia();
+        const final=media>=5&&media<7;
+        if(final) calculaFinal();
+        else{
+            if(media<5){
+                limpar(valorMedia);
+                const mensagemMediaHTML="<span><bold class='alerta'>Infelizmente você não pode fazer a final!</bold></span>";
+                valorMedia.innerHTML=mensagemMediaHTML;
+            }else{
+                const mensagemMediaHTML="<span>Parabéns, <i class='regular'>você passou!</i></span>";
+                valorMedia.innerHTML=mensagemMediaHTML;
+            }
+            }
     }
 }
-
-//ouvinte de eventos de entrada para cada campo de nota
+// Ouvinte de eventos de entrada para cada campo de nota
 notaUm.addEventListener("input", executa);
 notaDois.addEventListener("input", executa);
-
 executa();
