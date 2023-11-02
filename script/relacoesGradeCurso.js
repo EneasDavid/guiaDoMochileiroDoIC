d0ab58898204a6d1fab0d055da5f5355919cf1da
David Enéas <70227448+EneasDavid@users.noreply.github.com>
2023-11-02 12:33:11 -0300

    Adição de função para seleção de  eletivas; melhoria na estrutura do array 'materiaRelacoes' para portar a 'enfase' para posterior adição de funcionalidade

--- script/relacoesGradeCurso.js
+++ script/relacoesGradeCurso.js
@@ -1,225 +1,270 @@
 const materiasRelacoes = new Map([
     //PRIMEIRO PERIODO
-    //Programação 1
-    ["P1", {
-        depende: [],
-        dependencias: ["ED", "RC"],
-    }],
-    //Lógica Aplicada a Computação
-    ["LAC",{
-        depende: [],
-        dependencias: ["IA"],
-    }], 
-    //Cálculo Diferencial e Integral
-    ["CDI",{
-        depende: [],
-        dependencias: ["PE", "C3"],
-    }], 
-    //Sociedade, Computação e Ética
-    ["SCE",{
-        depende: [],
-        dependencias: [],
-    }], 
-    //Matemática Discreta
-    ["MD",{
-        depende: [],
-        dependencias: ["TG"],
-    }],
+        //Programação 1
+        ["P1", {
+            depende: [],
+            dependencias: ["ED", "RC"],
+        }],
+        //Lógica Aplicada a Computação
+        ["LAC",{
+            depende: [],
+            dependencias: ["IA"],
+        }], 
+        //Cálculo Diferencial e Integral
+        ["CDI",{
+            depende: [],
+            dependencias: ["PE", "C3"],
+        }], 
+        //Computação, Sociedade e Ética
+        ["CSE",{
+            depende: [],
+            dependencias: [],
+        }], 
+        //Matemática Discreta
+        ["MD",{
+            depende: [],
+            dependencias: ["TG"],
+        }],
     //SEGUNDA PERIODO
-    //Estrutura de Dados
-    ["ED", {
-        depende: ["P1"],
-        dependencias: ["TG", "P2", "P3", "PAA","IA","COMPILADORES"],
-    }],
-    //Banco de Dados
-    ["BD", {
-        depende: [],
-        dependencias:["P2","P3"],
-    }],
-    //Geometria Analítica
-    ["GA",{
-        depende: [],
-        dependencias:["AL"], 
-    }], 
-    //Organização e Arquitetura de Computadores
-    ["OAC",{
-        depende: [],
-        dependencias:["SO"],
-    }], 
+        //Estrutura de Dados
+        ["ED", {
+            depende: ["P1"],
+            dependencias: ["TG", "P2", "P3", "PAA","IA","COMPILADORES"],
+        }],
+        //Banco de Dados
+        ["BD", {
+            depende: [],
+            dependencias:["P2","P3"],
+        }],
+        //Geometria Analítica
+        ["GA",{
+            depende: [],
+            dependencias:["AL"], 
+        }], 
+        //Organização e Arquitetura de Computadores
+        ["OAC",{
+            depende: [],
+            dependencias:["SO"],
+        }], 
     //TERCEIRO PERIODO
-   //Teoria dos Grafos
-   ["TG", {
-        depende: ["ED", "MD"],
-        dependencias: [],
-    }],
-    //Redes de Computadores
-    ["RC",{
-        depende: ["P1"],
-        dependencias: ["P2","P3"],
-    }],
-    //Probabilidade e Estatistica
-    ["PE", {
-        depende: ["CDI"],
-        dependencias: [],
-    }],
-    //Álgebra Linear
-    ["AL",{
-        depende: ["GA"],
-        dependencias: [],
-    }],
+    //Teoria dos Grafos
+    ["TG", {
+            depende: ["ED", "MD"],
+            dependencias: [],
+        }],
+        //Redes de Computadores
+        ["RC",{
+            depende: ["P1"],
+            dependencias: ["P2","P3"],
+        }],
+        //Probabilidade e Estatistica
+        ["PE", {
+            depende: ["CDI"],
+            dependencias: [],
+        }],
+        //Álgebra Linear
+        ["AL",{
+            depende: ["GA"],
+            dependencias: [],
+        }],
     //QUARTO PERIODO
-    //Programação 2
-    ["P2",{
-        depende: ["ED", "RC", "BD"],
-        dependencias: [],
-    }],
-    //Programação 3
-    ["P3",{
-        depende: ["ED", "RC", "BD"],
-        dependencias: [],
-    }],
-    //Projeto de Análise de Algoritmos
-    ["PAA",{
-        depende: ["ED", "TG"],
-        dependencias: [],
-    }],
-    //Teoria da Computação
-    ["TC",{
-        depende: [],
-        dependencias: ["COMPILADORES"],
-    }],
+        //Programação 2
+        ["P2",{
+            depende: ["ED", "RC", "BD"],
+            dependencias: [],
+        }],
+        //Programação 3
+        ["P3",{
+            depende: ["ED", "RC", "BD"],
+            dependencias: [],
+        }],
+        //Projeto de Análise de Algoritmos
+        ["PAA",{
+            depende: ["ED", "TG"],
+            dependencias: [],
+        }],
+        //Teoria da Computação
+        ["TC",{
+            depende: [],
+            dependencias: ["COMPILADORES"],
+        }],
     //QUINTO PERIODO
-    //Compiladores
-    ["COMPILADORES",{
-        depende:  ["ED", "TC"],
-        dependencias: [],
-    }],
-    //Inteligência Artificial 
-    ["IA",{
-        depende: ["LAC", "ED"],
-        dependencias: [],
-    }],
-    //Sistemas Operacionais
-    ["SO",{
-        depende:  ["OAC"],
-        dependencias: [],
-    }],
-    //Computação Gráfica
-    ["CG",{
-        depende:  ["OAC"],
-        dependencias: [],
-    }],
+        //Compiladores
+        ["COMPILADORES",{
+            depende:  ["ED", "TC"],
+            dependencias: [],
+        }],
+        //Inteligência Artificial 
+        ["IA",{
+            depende: ["LAC", "ED"],
+            dependencias: [],
+        }],
+        //Sistemas Operacionais
+        ["SO",{
+            depende:  ["OAC"],
+            dependencias: [],
+        }],
+        //Computação Gráfica
+        ["CG",{
+            depende:  ["OAC"],
+            dependencias: [],
+        }],
     //SEXTO PERIODO
-    //Projeto e Desenvolvimento de Sistemas
-    ["PDS", {
-        depende: ["P1", "LAC", "CDI", "SCE", "MD", "ED", "BD", "GA", "OAC", "TG", "RC", "PE", "AL", "P2", "P3", "PAA", "TC", "COMPILADORES", "IA", "SO", "CG"],
-        dependencias: [],
-    }],//SETIMO PERIODO
-    //Metodologia de Pesquisa e Trabalho Individual
-    ["MPTI",{
-        depende: [],
-        dependencias: [],
-    }],
-    //Noções de Direito
-    ["ND",{
-        depende: [],
-        dependencias: [],
-    }],
+        //Projeto e Desenvolvimento de Sistemas
+        ["PDS", {
+            depende: ["P1", "LAC", "CDI", "SCE", "MD", "ED", "BD", "GA", "OAC", "TG", "RC", "PE", "AL", "P2", "P3", "PAA", "TC", "COMPILADORES", "IA", "SO", "CG"],
+            dependencias: [],
+        }],
+    //SETIMO PERIODO
+        //Metodologia de Pesquisa e Trabalho Individual
+        ["MPTI",{
+            depende: [],
+            dependencias: [],
+        }],
+        //Noções de Direito
+        ["ND",{
+            depende: [],
+            dependencias: [],
+        }],
     //OITAVO PERIODO
-    //Trabalho de Conclusão de Curso
-    ["TCC",{
-        depende: [],
-        dependencias: [],
-    }],
+        //Trabalho de Conclusão de Curso
+        ["TCC",{
+            depende: [],
+            dependencias: [],
+        }],
     //ELETIVAS
-    ["ELETIVA",{
-        depende: [],
-        dependencias: [],
-    }],
-    //Cálculo 3
-    ["C3", {
-        depende: ["CDI"],
-        dependencias: ["AM", "SD"],
-    }],
-    //Compiladores de Linguagens de Programação
-    ["CLP", {
-        depende: [],
-        dependencias: [],
-    }],
-    //Aprendizado de Máquina
-    ["AM",{
-        depende: ["C3"],
-        dependencias: [],
-    }],
-    // Sistemas Digitais
-    ["SD",{
-        depende: ["C3"],
-        dependencias: [],
-    }],
-    //Sistemas Distribuídos
-    ["SDI", {
-        depende: [],
-        dependencias: [],
-    }],
-    //Redes Neurais Artificiais e Processamento de Linguagem Natural
-    ["RNAP", {
-        depende: [],
-        dependencias: [],
-    }],
-    //FPGA
-    ["FPGA", {
-        depende: [],
-        dependencias: [],
-    }],
-    //Interação Homem-Máquina
-    ["IHM", {
-        depende: ["P3"],
-        dependencias: [],
-    }],
-    //Processamento Digital de Imagens
-    ["PDI",{
-        depende: ["PG"],
-        dependencias: [],
-    }],
-    //Computação Evolucionária
-    ["CE", {
-        depende: [],
-        dependencias: [],
-    }],
-    //Sistemas Embarcados
-    ["SE", {
-        depende: [],
-        dependencias: [],
-    }],
-    //Gerência de Projetos
-    ["GP", {
-        depende: ["PDS"],
-        dependencias: [],
-    }],
-    //Visão Computacional
-    ["VC", {
-        depende: [],
-        dependencias: [],
-    }],
-    //Ciência de Dados
-    ["CD",{
-        depende: ["PE"],
-        dependencias: [],
-    }],
-    //Microcontroladores e Aplicações
-    ["MA", {
-        depende: [],
-        dependencias: [],
-    }],
-    //Segurança de Sistemas Computacionais
-    ["SSC", {
-        depende: ["RC"],
-        dependencias: [],
-    }], 
+        ["ELETIVA",{
+            depende: [],
+            dependencias: [],
+        }],
+        //Cálculo 3
+        ["C3", {
+            depende: ["CDI"],
+            dependencias: ["AM", "SD"],
+            enfase: ["SInf", "SC", "CV"],
+        }],
+        //Compiladores de Linguagens de Programação
+        ["CLP", {
+            depende: [],
+            dependencias: [],
+            enfase: ["SInf", "SC", "CV"],
+        }],
+        //Aprendizado de Máquina
+        ["AM",{
+            depende: ["C3"],
+            dependencias: [],
+            enfase: ["SInf", "CV"],
+        }],
+        // Sistemas Digitais
+        ["SD",{
+            depende: ["C3"],
+            dependencias: [],
+            enfase: ["SC"],
+        }],
+        //Sistemas Distribuídos
+        ["SDI", {
+            depende: [],
+            dependencias: [],
+            enfase: ["SI"],
+        }],
+        //Redes Neurais Artificiais e Processamento de Linguagem Natural
+        ["RNAP", {
+            depende: [],
+            dependencias: [],
+            enfase: ["SInf", "SC", "CV"],
+        }],
+        //FPGA
+        ["FPGA", {
+            depende: [],
+            dependencias: [],
+        }],
+        //Interação Homem-Máquina
+        ["IHM", {
+            depende: ["P3"],
+            dependencias: [],
+            enfase: ["SI"],
+        }],
+        //Processamento Digital de Imagens
+        ["PDI",{
+            depende: ["PG"],
+            dependencias: [],
+        }],
+        //Computação Evolucionária
+        ["CE", {
+            depende: [],
+            dependencias: [],
+        }],
+        //Sistemas Embarcados
+        ["SE", {
+            depende: [],
+            dependencias: [],
+        }],
+        //Gerência de Projetos
+        ["GP", {
+            depende: ["PDS"],
+            dependencias: [],
+            enfase: ["SI"],
+        }],
+        //Visão Computacional
+        ["VC", {
+            depende: [],
+            dependencias: [],
+        }],
+        //Ciência de Dados
+        ["CD",{
+            depende: ["PE"],
+            dependencias: [],
+            enfase: ["SInf"],
+        }],
+        //Microcontroladores e Aplicações
+        ["MA", {
+            depende: [],
+            dependencias: [],
+        }],
+        //Segurança de Sistemas Computacionais
+        ["SSC", {
+            depende: ["RC"],
+            dependencias: [],
+            enfase: ["SI"],
+        }], 
 ]);
 
 const divMateria = document.querySelectorAll(".materia");
 const overlay = document.getElementById("overlay");
+
+const eletiva = document.querySelectorAll(".enfaseSeletor");
+const eletivasArmazena = new Map();
+
+eletiva.forEach((elemento) => {
+  elemento.addEventListener("click", () => {
+    const valorAtual = elemento.getAttribute("data-eletiva-relacoes");
+    // Obtém a fila existente para o elemento ou cria uma nova
+    let fila = eletivasArmazena.get(elemento) || [];
+    // Verifique se já existem duas divs selecionadas
+    if (eletivasArmazena.size >= 2) {
+      // Remove a classe "selecionado" da div mais antiga
+      const divMaisAntiga = eletivasArmazena.entries().next().value[0];
+      divMaisAntiga.classList.remove("selecionado");
+
+      // Remove o primeiro item do array
+      const primeiroItem = eletivasArmazena.get(divMaisAntiga);
+      primeiroItem.shift();
+      if (primeiroItem.length === 0) {
+        eletivasArmazena.delete(divMaisAntiga);
+      }
+    }
+    console.log(eletivasArmazena.size);
+    // Adicione o novo valor à fila
+    fila.push(valorAtual);
+    // Atualize a fila no objeto Map
+    eletivasArmazena.set(elemento, fila);
+    // Adicione a classe "selecionado" à div clicada
+    elemento.classList.add("selecionado");
+    for (const fila of eletivasArmazena.values()) {
+      console.log(fila);
+    }
+  });
+});
 
 const getMateriaSelecionadaRelacao = (materiaSelecionada) => {
     const { dependencias, depende } = materiasRelacoes.get(materiaSelecionada);
@@ -267,7 +312,6 @@ divMateria.forEach((elemento) => {
         const materiaSelecionadaRelacao = getMateriaSelecionadaRelacao(materiaSelecionada);
         if (materiaSelecionadaRelacao.dependencias.length > 0 || materiaSelecionadaRelacao.depende.length > 0) {
             removeHighlights();
-            console.log(materiaSelecionadaRelacao);
             highlightMateria(materiaSelecionadaRelacao);
             showOverlay();
         } else {
