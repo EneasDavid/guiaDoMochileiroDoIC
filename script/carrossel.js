6ff317b88be4b4bbc1844be5db75b9e5bec67653
Enéas <david00eneas@gmail.com>
2023-11-02 19:57:33 -0300

    Melhoria na div de 'enfases' junto ao aprimoramento do carrossel

--- script/carrossel.js
+++ script/carrossel.js
@@ -1,7 +1,16 @@
-$(document).ready(function(){
-    $('.carrossel').slick({
-      slidesToShow: 3, // Quantas divs são exibidas ao mesmo tempo
-      slidesToScroll: 1, // Quantas divs são roladas de cada vez
-    });
-  });
-  
+var mySwiper = new Swiper('.swiper-container', {
+  slidesPerView: 2, // Número fixo de slides visíveis
+  spaceBetween: 5, // Espaçamento entre os slides
+  loop: true, // Habilitar o loop
+  speed: 500, // Velocidade da animação
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 4, // Número de slides visíveis em telas maiores
+      freeMode: false, // Desabilitar o modo de rolagem livre
+    }
+  }
+});
